f55ac1a1467683f6cd8811cc8333caac5ea6e45d
Terry Lee <uwgdw88@gmail.com>
2024-06-20 13:47:35 -0400

    refacted the file structures

--- backend/deploy/deploy.sh
+++
@@ -1,3 +0,0 @@
-cd ..
-./gradlew clean build
-scp -i /Users/terrylee/Workspace/myKeyPair.pem -r build/libs/*jar ec2-user@18.223.10.154:/home/ec2-user/
--- frontend/deploy/deploy.sh
+++
@@ -1,3 +0,0 @@
-npm run build
-ssh -i /Users/terrylee/Workspace/myKeyPair.pem ec2-user@18.223.10.154 "rm -r /home/ec2-user/build/*"
-scp -i /Users/terrylee/Workspace/myKeyPair.pem -r ../build/* ec2-user@18.223.10.154:/home/ec2-user/build
